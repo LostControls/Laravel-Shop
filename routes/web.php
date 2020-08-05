4ec0a16c8f9701569ca7f48aa99bb8d5cd62643f
ChenYiWen <1447292956@qq.com>
2020-08-05 10:16:41 +0800

    邮箱验证功能

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PagesController@root')->name('root');
-Auth::routes();
+Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
+Auth::routes(['verify' => true]);
 
 
