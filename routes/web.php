1ad9dc0ab253d94217b7efdfd7100a73dcd98129
ChenYiWen <1447292956@qq.com>
2020-08-11 17:48:14 +0800

    修改和删除收货地址

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,11 @@ Route::group(['middleware' => ['auth','verified']], function () {
     Route::get('user_addresses/create','UserAddressesController@create')->name('user_addresses.create');
     //处理添加收货地址
     Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
+    //修改收货地址
+    Route::get('user_addresses/{user_address}','UserAddressesController@edit')->name('user_addresses.edit');
+    Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
+    //删除收货地址
+    Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
 });
 
 
