6545c869d32a8852f231183d522087d323b1d49a
ChenYiWen <1447292956@qq.com>
2020-08-18 11:01:11 +0800

    异常处理

---
+++ app/Exceptions/InvalidRequestException.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Illuminate\Http\Request;
+use think\View;
+
+class InvalidRequestException extends Exception
+{
+    public function __construct(string $message = "", $code = 400)
+    {
+        parent::__construct($message, $code);
+    }
+
+    public function render(Request $request)
+    {
+        if ($request->expectsJson()) {
+            // json() 方法第二个参数就是 Http 返回码
+            return response()->json(['msg' => $this->message], $this->code);
+        }
+
+        return view('pages.error', ['msg' => $this->message]);
+    }
+}
