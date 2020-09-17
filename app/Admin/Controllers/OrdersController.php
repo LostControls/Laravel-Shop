0123269bfef7c4c906cff7b8644c56d9463420f3
ChenYiWen <1447292956@qq.com>
2020-09-17 14:57:33 +0800

    管理后台 - 订单详情

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -7,6 +7,7 @@ use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Show;
+use Encore\Admin\Layout\Content;
 
 class OrdersController extends AdminController
 {
@@ -115,5 +116,13 @@ class OrdersController extends AdminController
         $form->textarea('extra', __('Extra'));
 
         return $form;
+    }
+
+    public function show($id, Content $content)
+    {
+        return $content
+            ->header('查看订单')
+            // body 方法可以接受 Laravel 的视图作为参数
+            ->body(view('admin.orders.show', ['order' => Order::find($id)]));
     }
 }
