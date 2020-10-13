74b4acb0de5b943cc4c9cda86e161db815e34b81
ChenYiWen <1447292956@qq.com>
2020-10-13 10:31:03 +0800

    商品类目管理后台

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -37,4 +37,12 @@ Route::group([
     $router->put('coupon_codes/{id}', 'CouponCodesController@update');
     // 删除优惠券
     $router->delete('coupon_codes/{id}', 'CouponCodesController@destroy');
+
+    $router->get('categories', 'CategoriesController@index');
+    $router->get('categories/create', 'CategoriesController@create');
+    $router->get('categories/{id}/edit', 'CategoriesController@edit');
+    $router->post('categories', 'CategoriesController@store');
+    $router->put('categories/{id}', 'CategoriesController@update');
+    $router->delete('categories/{id}', 'CategoriesController@destroy');
+    $router->get('api/categories', 'CategoriesController@apiIndex');
 });
