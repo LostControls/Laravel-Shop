ad16923f75e7cbac19db2640eb5748d5598a7ac1
ChenYiWen <1447292956@qq.com>
2020-10-27 11:44:45 +0800

    众筹商品详情页

--- routes/web.php
+++ routes/web.php
@@ -93,6 +93,9 @@ Route::group(['middleware' => ['auth','verified']], function () {
 
     // 检查优惠券
     Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
+
+    // 众筹
+    Route::post('crowdfunding_orders','OrdersController@crowdfunding')->name('crowdfunding_orders.store');
 });
 
 // 服务器端回调地址
