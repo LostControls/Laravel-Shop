7d85f0066bf6cf47f5a30c27bf1ac1b9643ef6d6
ChenYiWen <1447292956@qq.com>
2020-10-27 14:43:43 +0800

    订单模块调整

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -35,6 +35,7 @@ class OrderService
                 ],
                 'remark'       => $remark,
                 'total_amount' => 0,
+                'type'         => Order::TYPE_NORMAL,
             ]);
             // 订单关联到当前用户
             $order->user()->associate($user);
@@ -105,6 +106,7 @@ class OrderService
                 ],
                 'remark'  => '',
                 'total_amount' => $sku->price * $amount,
+                'type'              => Order::TYPE_CROWDFUNDING,
             ]);
             // 订单关联到当前用户
             $order->user()->associate($user);
--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -49,6 +49,7 @@
             <!-- 订单发货开始 -->
             <!-- 如果订单未发货，展示发货表单 -->
             @if($order->ship_status === \App\Models\Order::SHIP_STATUS_PENDING)
+                @if($order->refund_status !== \App\Models\Order::REFUND_STATUS_SUCCESS &&  ($order->type !== \App\Models\Order::TYPE_CROWDFUNDING || $order->items[0]->product->crowdfunding->status === \App\Models\CrowdfundingProduct::STATUS_SUCCESS))
                 <tr>
                     <td colspan="4">
                         <form action="{{ route('admin.orders.ship', [$order->id]) }}" method="post" class="form-inline">
@@ -76,6 +77,7 @@
                         </form>
                     </td>
                 </tr>
+                @endif
             @else
                 <!-- 否则展示物流公司和物流单号 -->
                 <tr>
