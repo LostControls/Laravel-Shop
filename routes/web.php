aed7173ea71cecff99d48a566309710b5e5b5589
ChenYiWen <1447292956@qq.com>
2020-10-29 11:30:46 +0800

    创建分期付款

--- routes/web.php
+++ routes/web.php
@@ -96,6 +96,9 @@ Route::group(['middleware' => ['auth','verified']], function () {
 
     // 众筹
     Route::post('crowdfunding_orders','OrdersController@crowdfunding')->name('crowdfunding_orders.store');
+
+    // 分期付款
+    Route::post('payment/{order}/installment', 'PaymentController@payByInstallment')->name('payment.installment');
 });
 
 // 服务器端回调地址
