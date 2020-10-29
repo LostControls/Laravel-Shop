36aa9ecb809fb68fec4bea1c10f7ebf175d8f23b
ChenYiWen <1447292956@qq.com>
2020-10-29 09:49:56 +0800

    初始化分期付款模型

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -6,3 +6,8 @@ function testHelper() {
 function routeClass() {
 	return str_replace('.', '-', Route::currentRouteName());
 }
+
+// 默认的精度为小数点后两位
+function big_number($number, $scale = 2) {
+    return new \Moontoast\Math\BigNumber($number,$scale);
+}
